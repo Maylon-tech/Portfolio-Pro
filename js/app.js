a053609c844a2a1036e4e0817c8866570514d8ce
Maylon-tech <minatokiroisen77@gmail.com>
2022-10-29 10:18:59 +0900

    build protfli+projects+reviews

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@ const menu = document.querySelector(".menu-icon")
 menu.onclick = () => {
     menu.classList.toggle('move')
 }
+
+// Swiper codes  - Reviews Slider autoplay
+
+let swiper = new Swiper(".review-content", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        
+      })
